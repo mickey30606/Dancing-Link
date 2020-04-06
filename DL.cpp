2e47a6ba4abff65b2de0c2f1af73dfd6f953a7e1
Mickey Tseng <mickey30606@gmail.com>
2020-04-06 18:42:34 +0800

    做完行的插入 但沒有測試 記得要做每一列的個數計數

--- DL.cpp
+++ DL.cpp
@@ -6,6 +6,7 @@ using namespace std;
 bool DLX::init(int number){
     number++;
     standard_row = (Node **) calloc(number, sizeof(Node *));
+    sum_row = (int *) calloc(number, sizeof(int));
     Node *tmp_left = NULL;
     head = NULL;
     tmp_left = head = add_standard_node(tmp_left, head, 0);
@@ -48,5 +49,64 @@ void DLX::check_standard_node(Node *head){
 }
 
 bool DLX::add_member_node(int x, int y, int number){
+    //每一種的問題這邊都不一樣 因為數獨共4個條件所以每個數字會跑4次
+    int k=0;    //用來計數的
+    if(number != 0){
+        Row_tmp[k++] = x*9 + y +1;    //1~81
+        Row_tmp[k++]= 9*9 + x*9 + number;    //在一個橫排裡有什麼數字
+        Row_tmp[k++] = 2*9*9 + y*9 + number;    //在一個直行裡有什麼數字
+        Row_tmp[k++] = 3*9*9 + x/3*3*9 + y/3*9 + number;    //在一個宮格里有什麼數字
+        add_member_row(Row_tmp, k, x, y, number);
+        k=0;
+    }else{
+        for(int i=1;i<10;i++){
+            Row_tmp[k++] = x*9 + y +1;    //1~81
+            Row_tmp[k++]= 9*9 + x*9 + i;    //在一個橫排裡有什麼數字
+            Row_tmp[k++] = 2*9*9 + y*9 + i;    //在一個直行裡有什麼數字
+            Row_tmp[k++] = 3*9*9 + x/3*3*9 + y/3*9 + i;    //在一個宮格里有什麼數字
+            add_member_row(Row_tmp, k, x, y, i);
+            k=0;
+        }
+    }
 
+    return 1;
+}
+
+void DLX::add_member_row(int Row[], int k, int x, int y, int number){
+    Node *tmp[k];
+    for(int i=0;i<k;i++){
+        tmp[i] = member_node(Row[i], x, y, number);
+    }
+    for(int i=0;i<k;i++){
+        if(i==0){
+            tmp[i]->right = tmp[i+1];
+            tmp[i]->left = tmp[i+k-1];
+        }else if(i == k-1){
+            tmp[i]->right = tmp[0];
+            tmp[i]->left = tmp[i-1];
+        }else{
+            tmp[i]->right = tmp[i+1];
+            tmp[i]->left = tmp[i-1];
+        }
+    }
+
+    return;
+}
+
+Node * DLX::member_node(int where, int x, int y, int number){
+    Node *p = new Node();
+    p->left = p->right = p;
+    p->RowHead = standard_row[where];
+    p->x = x, p->y = y, p->number = number;
+    if(sum_row[where] == 0){
+        standard_row[where]->down = standard_row[where]->up = p;
+        p->up = p->down = standard_row[where];
+    }else{
+        p->up = standard_row[where]->up;
+        p->down = standard_row[where];
+        standard_row[where]->up->down = p;
+        standard_row[where]->up = p;
+    }
+
+    return p;
 }
--- DL.h
+++ DL.h
@@ -19,8 +19,12 @@ class DLX{
         Node* add_standard_node(Node *, Node *, int);
         void check_standard_node(Node *);
         bool add_member_node(int, int, int);
+        void add_member_row(int [], int, int, int, int);
+        Node* member_node(int, int, int, int);
     private:
         Node *head;    //這個一定要 攸關後面檢查完成條件
-        Node **standard_row;
+        Node **standard_row;    //用來快速尋找standard_row 不然要從head慢慢推 程式應該會變很慢
+        int Row_tmp[4];    //因為數獨有4個條件 所以設4個來暫存要連到的standard row
+        int *sum_row;
 };
 #endif
